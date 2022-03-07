d045cc41c3ecbaedd00cc3445e317388b4a4ceb7
wsrtka <witold.serwatka@pm.me>
2022-03-07 07:54:57 +0100

    add utils

--- main.py
+++ main.py
@@ -21,15 +21,7 @@ APARTMENTS_TABLE_NAME = ...
 
 @functions_framework.http
 def post_apartment(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
+    """HTTP Cloud Function for posting new apartments."""
     # parse request
     request_json = request.get_json()
 
