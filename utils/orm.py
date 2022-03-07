7b0eb895843fe0bde0e70a2e39734a2e67a4c572
wsrtka <witold.serwatka@pm.me>
2022-03-07 13:25:36 +0100

    add voivodeship enum

--- utils/orm.py
+++ utils/orm.py
@@ -1,13 +1,37 @@
 """Module containing ORM."""
 
 import uuid
+import enum
 
-from sqlalchemy import Column, Integer
+import sqlalchemy.sql.functions as func
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
+
+
+class VoivodeshipEnum(enum.Enum):
+    """Class representing voivodeship enum in database."""
+
+    GREATER_POL = "Greater Poland"
+    KUYAVIA = "Kuyavia-Pomerania"
+    LESSER_POL = "Lesser Poland"
+    LODZ = "Łódź"
+    LOWER_SIL = "Lower Silesia"
+    LUBLIN = "Lublin"
+    LUBUSZ = "Lubusz"
+    MASOVIA = "Masovia"
+    OPOLE = "Opole"
+    PODLASKIE = "Podlaskie"
+    POMERANIA = "Pomerania"
+    SILESIA = "Silesia"
+    SUBCARPATHIA = "Subcarpathia"
+    HOLY_CROSS = "Holy Cross Province"
+    WARMIA = "Warmia-Masuria"
+    WEST_POM = "West Pomerania"
 
 
 # pylint: disable=too-few-public-methods
@@ -18,5 +42,10 @@ class Apartment(Base):
 
     id = Column("id", Integer, primary_key=True)
     guid = Column("guid", UUID(as_uuid=True), default=uuid.uuid4)
-    created_at = Column("created_at", TIMESTAMP)
-    update_at = Column("updated_at", TIMESTAMP)
+    created_at = Column("created_at", TIMESTAMP, server_default=func.now())
+    updated_at = Column("updated_at", TIMESTAMP, onupdate=func.now())
+    city = Column("city", String(50))
+    zip = Column("zip", String(10), nullable=False)
+    voivodeship = Column(
+        "voivodeship",
+    )
