5be5c9f58e817bc90097c0f1a66ad3368acb0cac
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-03-16 17:15:55 +0100

    add the old create_host because pylint isn't letting me merge

--- functions/host.py
+++ functions/host.py
@@ -4,6 +4,7 @@ import flask
 from sqlalchemy import select
 from utils.db import get_db_session
 from utils import orm
+from utils.payload_parser import parse, HostParser
 
 
 def handle_get_all_hosts(request):
@@ -14,3 +15,20 @@ def handle_get_all_hosts(request):
         response = [host.to_json() for host in result.scalars()]
 
     return flask.Response(response=response, status=200)
+
+
+def handle_create_host(request):
+    data = request.get_json(silent=True)
+    result = parse(data, HostParser)
+    if not result.success:
+        return f"Failed: {','.join(result.errors)}", 405
+
+    # TODO: proper logs
+    if result.warnings:
+        print(result.warnings)
+
+    Session = get_db_session()
+    with Session() as session:
+        session.add(result.payload)
+        session.commit()
+        return "Success", 201
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from functions import host
 from functions import guest
 
 
-
 @functions_framework.http
 def add_accommodation(request):
     """HTTP Cloud Function for posting new accommodation units."""
@@ -23,11 +22,6 @@ def get_all_accommodations(request):
 
 
 @functions_framework.http
-def create_host(request):
-    return handle_create_host(request)
-
-
-@functions_framework.http
 def get_all_guests(request):
     """HTTP Cloud Function for getting all guests."""
     return guest.handle_get_all_guests(request)
@@ -38,12 +32,20 @@ def add_guest(request):
     """HTTP Cloud Function for posting new guests."""
     return guest.handle_add_guest(request)
 
+
 @functions_framework.http
 def get_guest_by_id(request):
     """HTTP Cloud Function for getting guest by id."""
     return guest.handle_get_guest_by_id(request)
 
+
 @functions_framework.http
 def get_all_hosts(request):
     """HTTP Cloud Function for getting all hosts."""
     return host.handle_get_all_hosts(request)
+
+
+@functions_framework.http
+def create_host(request):
+    """HTTP Cloud Function for posting a new host."""
+    return handle_create_host(request)
