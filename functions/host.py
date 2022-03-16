595c01c49ddb416efaa69aaae3a7e056921a1142
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-03-16 15:55:39 +0100

    change from id to guid

--- functions/host.py
+++ functions/host.py
@@ -17,18 +17,23 @@ def handle_get_all_hosts(request):
 
 
 def handle_get_host_by_id(request):
-    id = request.args.get("hostId")
-    if id is None or not id.isdigit():
-        return flask.Response(response=f"Received invalid hostId: {id}", status=405)
+    try:
+        id = request.args.get("hostId")
+    except ValueError:
+        return flask.Response(response=f"Received invalid hostId: {id}", status=400)
+    if id is None:
+        return flask.Response(response="Received no hostId", status=400)
 
     Session = get_db_session()
     with Session() as session:
         try:
-            host = session.query(orm.Host).get(id)
-            if host is None:
+            stmt = select(orm.Host).where(orm.Host.guid == id)
+            res = session.execute(stmt)
+            response = [host.to_json() for host in res.scalars()]
+            if len(response) == 0:
                 return flask.Response(
                     response=f"Host with id = {id} not found", status=404
                 )
-            return flask.Response(response=host.to_json(), status=200)
+            return flask.Response(response=response, status=200)
         except TypeError as e:
             return flask.Response(response=f"Received invalid hostId: {e}", status=400)
