f58f670b91e687d1b6466c870f62b81906ba13eb
Michał Łazowik <mlazowik@gmail.com>
2022-03-18 19:15:52 +0100

    Fix priority status enum (#78)
    * Fix priority status enum
    
    * Fix more statuses

--- utils/orm.py
+++ utils/orm.py
@@ -53,9 +53,9 @@ class Status(str, enum.Enum):
 class PriorityStatus(str, enum.Enum):
     """Class representing status enum in database."""
 
-    DOES_NOT_RESPOND = "created"
-    ACCOMMODATION_NOT_NEEDED = "verified"
-    EN_ROUTE_UA = "banned"
+    DOES_NOT_RESPOND = "does_not_respond"
+    ACCOMMODATION_NOT_NEEDED = "accommodation_not_needed"
+    EN_ROUTE_UA = "en_route_ua"
     EN_ROUTE_PL = "en_route_pl"
     IN_KRK = "in_krk"
     AT_R3 = "at_r3"
