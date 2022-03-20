4923d16580d26320a9d98dae6bd06aeea1898dac
Michał Łazowik <mlazowik@gmail.com>
2022-03-20 03:47:25 +0100

    Change lang codes capitalization

--- Database/03_InsertMainDictionaryData.sql
+++ Database/03_InsertMainDictionaryData.sql
@@ -1,8 +1,8 @@
 INSERT INTO public.languages("name", code2, code3)
-VALUES('English',	'En',	'Eng'),
-('Ukrainian',	'Uk',	'Ukr'),
-('Polish',	'Pl',	'Pol'),
-('Russian',	'Ru',	'Rus');
+VALUES('English',	'en',	'eng'),
+('Ukrainian',	'uk',	'ukr'),
+('Polish',	'pl',	'pol'),
+('Russian',	'ru',	'rus');
 
 
 
