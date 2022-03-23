03b13123e7f7a3474550c24e87fcfdd59f9d8d00
nanvel <1374878+nanvel@users.noreply.github.com>
2022-03-23 13:42:56 +0700

    test

--- tests/fixtures/db.sql
+++ tests/fixtures/db.sql
@@ -8,4 +8,10 @@ VALUES ('Ewelina Głuszek', '443 143 258', 'wdonx.e@o2.pl', '17:30', '18:00', ''
        ('Edmund Królikowski', '550.343.606', 'dqova@poczta.onet.pl', '7:10', '8:00', '',
         '2078dad6-5dc9-4e5a-8ee0-d69c44f460e2');
 
+DELETE FROM guests;
+
 DELETE FROM users;
+
+INSERT INTO users (guid, given_name, family_name, email, google_sub, google_picture)
+VALUES ('782962fc-dc11-4a33-8f08-b7da532dd40d', 'John', 'Doe',
+        'john.doe@example.com', '10769150350006150715113082367', 'https://img.google.com/1.jpg');
