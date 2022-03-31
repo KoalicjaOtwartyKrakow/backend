a4212ef552f0efc93e97c6fdcc5770d91e749cc2
Michał Łazowik <mlazowik@gmail.com>
2022-03-31 17:29:24 +0200

    Set connections limit back to 1, add comment explaining why (#139)

--- kokon/utils/db.py
+++ kokon/utils/db.py
@@ -16,7 +16,8 @@ class DB:
         """Get database engine."""
         if cls._db_connection_pool is None:
             cls._db_connection_pool = sqlalchemy.create_engine(
-                # See https://cloud.google.com/sql/docs/postgres/connect-functions#connect_to
+                # See:
+                # https://cloud.google.com/sql/docs/postgres/connect-functions#connect_to
                 sqlalchemy.engine.url.URL.create(
                     drivername="postgresql+pg8000",
                     username=settings.DB_USER,  # e.g. "my-database-user"
@@ -24,7 +25,19 @@ class DB:
                     database=settings.DB_NAME,  # e.g. "my-database-name"
                     query=settings.DB_QUERY,
                 ),
-                pool_size=5,
+                # Cloud SQL imposes a maximum limit on concurrent connections, and these
+                # limits may vary depending on the database engine chosen (see Cloud SQL
+                # Quotas and Limits). It's recommended to use a connection with Cloud
+                # Functions, but it is important to set the maximum number of
+                # connections to 1.
+                #
+                # Note: Cloud Functions limits concurrent executions to one per
+                # instance. You never have a situation where a single function instance
+                # is processing two requests at the same time. In most situations, only
+                # a single database connection is needed.
+                #
+                # https://cloud.google.com/sql/docs/mysql/connect-functions#connection-limits
+                pool_size=1,
                 max_overflow=0,
             )
             print(f"Connecting to query={settings.DB_QUERY}, name={settings.DB_NAME}")
