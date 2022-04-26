68b8476592058a268d4c28fb05872e8b9984073a
nanvel <1374878+nanvel@users.noreply.github.com>
2022-04-26 21:23:38 +0700

    initial

---
+++ kokon/orm/host_verification_session.py
@@ -0,0 +1,28 @@
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID as DB_UUID
+from sqlalchemy.orm import relationship
+
+from .base import Base
+
+
+class HostVerificationSession(Base):
+    __tablename__ = "host_verification_sessions"
+
+    id = sa.Column(
+        "id",
+        DB_UUID(as_uuid=True),
+        server_default=sa.text("uuid_generate_v4()"),
+        primary_key=True,
+    )
+    host_id = sa.Column(
+        "host_id",
+        sa.ForeignKey("hosts.guid", name="fk_host_verification_sessions_host_id"),
+        nullable=False,
+    )
+    host = relationship("Host", back_populates="verifications", foreign_keys=[host_id])
+    conversation_id = sa.Column("conversation_id", sa.Integer, nullable=False)
+    first_name = sa.Column("first_name", sa.String(100), nullable=True)
+    last_name = sa.Column("last_name", sa.String(100), nullable=True)
+
+    def __repr__(self):
+        return f"HostVerificationSession: {self.__dict__}"
