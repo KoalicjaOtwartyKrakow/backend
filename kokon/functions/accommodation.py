0be12f9335a4f0024ade5d156e8ece00bc987672
Juliusz Ryłko <juliusz.rylko@it-team.salamlab.pl>
2022-04-27 00:21:45 +0200

    removed old TODOs

--- kokon/functions/accommodation.py
+++ kokon/functions/accommodation.py
@@ -102,7 +102,6 @@ def handle_get_accommodation_by_id(request: Request):
     return JSONResponse(response, status=200)
 
 
-# TODO-BACKEND-92
 def handle_update_accommodation(request: Request):
     schema = AccommodationUnitSchema()
     schema_full = AccommodationUnitSchemaFull()
